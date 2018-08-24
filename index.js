79332f273e13bf60c5bdce34bb549c4911dac7e6
András Demszky <andrasdemszky@Andrass-MacBook-Pro.local>
2018-08-24 14:55:40 +0200

    margin in number

--- index.js
+++ index.js
@@ -20,17 +20,21 @@ $(document).ready(function () {
                 var result
                 if ($('#inputGroupSelect01').val() == 'Percentage') {
                     valuetype = 'Cost'
+                    marginText = 'Margin in Number'
                     result = $('#price').val() * (1 - $('#margin').val() / 100)
+                    marginpercent = $('#price').val() * $('#margin').val() / 100
                     $('#valuetype').text(valuetype)
                     $('#result').text(result)
+                    $('#marginText').text(marginText)
+                    $('#marginpercent').text(marginpercent)
                 }
                 if ($('#inputGroupSelect01').val() == 'Number') {
                     valuetype = 'Cost'
-                    marginText='Margin in %'
+                    marginText = 'Margin in %'
                     var x = Number($('#price').val())
                     var y = Number($('#margin').val())
-                    result = x-y
-                    marginpercent=y/x*100
+                    result = x - y
+                    marginpercent = y / x * 100
                     $('#valuetype').text(valuetype)
                     $('#result').text(result)
                     $('#marginText').text(marginText)
@@ -39,26 +43,34 @@ $(document).ready(function () {
             }
             if ($('#margin').val() == '') {
                 var result
-                valuetype = 'Margin'
+                valuetype = 'Margin in %'
+                marginText = 'Margin in Number'
                 result = (1 - $('#cost').val() / ($('#price').val())) * 100
+                marginpercent = $('#price').val() * result / 100
                 $('#valuetype').text(valuetype)
                 $('#result').text(result + '%')
+                $('#marginText').text(marginText)
+                $('#marginpercent').text(marginpercent)
             }
             if ($('#price').val() == '') {
                 var result
                 if ($('#inputGroupSelect01').val() == 'Percentage') {
                     valuetype = 'Price'
+                    marginText = 'Margin in Number'
                     result = $('#cost').val() / (1 - $('#margin').val() / 100)
+                    marginpercent = result * $('#margin').val() / 100
                     $('#valuetype').text(valuetype)
                     $('#result').text(result)
+                    $('#marginText').text(marginText)
+                    $('#marginpercent').text(marginpercent)
                 }
                 if ($('#inputGroupSelect01').val() == 'Number') {
                     valuetype = 'Price'
-                    marginText='Margin in %'
+                    marginText = 'Margin in %'
                     var x = Number($('#cost').val())
                     var y = Number($('#margin').val())
-                    result = x+y
-                    marginpercent=y/result*100
+                    result = x + y
+                    marginpercent = y / result * 100
                     $('#valuetype').text(valuetype)
                     $('#result').text(result)
                     $('#marginText').text(marginText)
